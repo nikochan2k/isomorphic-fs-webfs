b98d5b918fd67919265769c2a8bfa3484815208a
Yoshihiro Seki <nikochan2k@gmail.com>
2021-08-09 23:59:09 +0900

    modify buffer.ts for chunking

--- src/webfs/buffer.ts
+++ src/webfs/buffer.ts
@@ -1,4 +1,4 @@
-import { util } from "isomorphic-fs";
+import { DEFAULT_BUFFER_SIZE, util } from "isomorphic-fs";
 
 export function dataUrlToBase64(dataUrl: string) {
   const index = dataUrl.indexOf(",");
@@ -13,35 +13,67 @@ async function blobToBase64(blob: Blob): Promise<string> {
     return "";
   }
 
-  const base64 = await new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = function (ev) {
-      reject(reader.error || ev);
-    };
-    reader.onload = function () {
-      const base64 = dataUrlToBase64(reader.result as string);
-      resolve(base64);
-    };
-    reader.readAsDataURL(blob);
-  });
-  return base64;
+  const chunks: string[] = [];
+  for (
+    let start = 0, end = blob.size;
+    start < end;
+    start += DEFAULT_BUFFER_SIZE
+  ) {
+    const blobChunk = blob.slice(start, start + DEFAULT_BUFFER_SIZE);
+    const chunk = await new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = function (ev) {
+        reject(reader.error || ev);
+      };
+      reader.onload = function () {
+        const base64 = dataUrlToBase64(reader.result as string);
+        resolve(base64);
+      };
+      reader.readAsDataURL(blobChunk);
+    });
+    chunks.push(chunk);
+  }
+  return chunks.join("");
+}
+
+async function concatArrayBuffers(chunks: ArrayBuffer[], byteLength: number) {
+  const u8 = new Uint8Array(byteLength);
+  let pos = 0;
+  for (const chunk of chunks) {
+    u8.set(new Uint8Array(chunk), pos);
+    pos += chunk.byteLength;
+  }
+  return u8.buffer;
 }
 
 async function blobToArrayBufferUsingReadAsArrayBuffer(blob: Blob) {
   if (blob.size === 0) {
     return new ArrayBuffer(0);
   }
-  const buffer = await new Promise<ArrayBuffer>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = (ev) => {
-      reject(reader.error || ev);
-    };
-    reader.onload = () => {
-      resolve(reader.result as ArrayBuffer);
-    };
-    reader.readAsArrayBuffer(blob);
-  });
-  return buffer;
+  let byteLength = 0;
+  const chunks: ArrayBuffer[] = [];
+  for (
+    let start = 0, end = blob.size;
+    start < end;
+    start += DEFAULT_BUFFER_SIZE
+  ) {
+    const blobChunk = blob.slice(start, start + DEFAULT_BUFFER_SIZE);
+    const chunk = await new Promise<ArrayBuffer>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = (ev) => {
+        reject(reader.error || ev);
+      };
+      reader.onload = () => {
+        const chunk = reader.result as ArrayBuffer;
+        byteLength += chunk.byteLength;
+        resolve(chunk);
+      };
+      reader.readAsArrayBuffer(blobChunk);
+    });
+    chunks.push(chunk);
+  }
+
+  return concatArrayBuffers(chunks, byteLength);
 }
 
 async function blobToArrayBufferUsingReadAsDataUrl(blob: Blob) {
