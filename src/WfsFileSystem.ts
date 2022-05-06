0c6b2ddba3da60ca5423ca06aa0c7f63329ae546
Yoshihiro Seki <nikochan2k@gmail.com>
2022-05-06 21:14:11 +0900

    add polyfill

--- src/WfsFileSystem.ts
+++ src/WfsFileSystem.ts
@@ -18,6 +18,25 @@ import {
 import { WfsDirectory } from "./WfsDirectory";
 import { WfsFile } from "./WfsFile";
 
+if (!Promise.allSettled) {
+  /* eslint-disable */
+  (Promise as any).allSettled = (promises: any) =>
+    Promise.all(
+      promises.map((p: any) =>
+        p
+          .then((value: any) => ({
+            status: "fulfilled",
+            value,
+          }))
+          .catch((reason: any) => ({
+            status: "rejected",
+            reason,
+          }))
+      )
+    );
+  /* eslint-enable */
+}
+
 const requestFileSystem =
   window.requestFileSystem || (window as any).webkitRequestFileSystem; // eslint-disable-line
 export class WfsFileSystem extends AbstractFileSystem {
